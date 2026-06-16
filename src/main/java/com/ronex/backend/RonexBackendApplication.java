1293634d12c332695975b26738bcdd4d675aaed6
Venkat <hr.venky01@gmail.com>
2026-06-16 09:48:16 +0530

    Initial Ronex backend commit

---
+++ src/main/java/com/ronex/backend/RonexBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ronex.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RonexBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RonexBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ronex/backend/RonexBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronex.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RonexBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
